075fc1c0bd1f63df0bd01f81f22f05f58a89e603
Patrick-Star-CN <1592810043@qq.com>
2022-03-25 16:44:01 +0800

    update backend

--- backend/new.js
+++ backend/new.js
@@ -14,19 +14,38 @@ io.use((socket, next) => {
   next();
 });
 
-io.on('connection', function (socket) {
+io.on('connection', socket => {
+  const nameThis;
   console.log('user connected');
-  socket.on('disconnect', function (socket) {
-    console.log(socket);
+
+  socket.on('disconnect', socket => {
+    userAll.delete(name, socket);
+    console.log(`${name} disconnected`);
+    io.sockets.emit('online', [...userAll.keys()]);
   });
 
-  socket.on('login', (name) => {
-    console.log(name + ' has logined.');
-    io.sockets.emit('online', 'good');
+  socket.on('login', (name fn) => {
+    if(!name) {
+      fn('EMPTY_NAME_ERROR');
+    } else if(userAll.get(name) !== undefined) {
+      fn('SAME_NAME_ERROR');
+    } else {
+      console.log(name + ' has logined.');
+      nameThis = name;
+      userAll.set(name, socket);
+      io.sockets.emit('online', [...userAll.keys()]);
+      fn('success');
+    }
   });
 
   socket.on('sendMessage', (content) => {
-    console.log(' send  a message :' + content);
+    console.log('receive a message', name, content);
+    const message = {
+      time: Date.now(),
+      sender: name,
+      content
+    };
+    socket.broadcast.to(roomid).emit('receiveMessage', message);
   });
 });
 
